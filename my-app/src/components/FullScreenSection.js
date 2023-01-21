86fc84070cb21dd1f2714b7870c11e8b94af21ee
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2023-01-21 10:03:14 +0800

    Update FullScreenSection.js

--- my-app/src/components/FullScreenSection.js
+++ my-app/src/components/FullScreenSection.js
@@ -6,7 +6,7 @@ const FullScreenSection = ({ children, isDarkBackground, ...boxProps }) => {
       backgroundColor={boxProps.backgroundColor}
       color={isDarkBackground ? "white" : "black"}
     >
-      
+        
       <VStack maxWidth="1280px" minHeight="100vh" {...boxProps}>
         {children}
       
