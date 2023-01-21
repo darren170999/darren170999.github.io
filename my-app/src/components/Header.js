b8d2f509753e878d34d305bc884602df81b5f098
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2023-01-21 10:42:47 +0800

    Update Header.js

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,13 +1,14 @@
 import React, { useEffect, useRef } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; //
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons"; //
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import {
   faGithub,
   faLinkedin,
   faSquareReddit,
   faGoogleDrive,
-} from "@fortawesome/free-brands-svg-icons"; //
+} from "@fortawesome/free-brands-svg-icons";
 import { Box, HStack } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 
 const socials = [
   {
@@ -33,38 +34,20 @@ const socials = [
 ];
 
 const Header = () => {
-  const handleClick = (anchor) => () => {
-    const id = `${anchor}-section`;
-    const element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-    }
-  };
-
   return (
     <Box
-      position="fixed"
-      top={0}
-      left={0}
-      right={0}
-      translateY={0}
-      transitionProperty="transform"
-      transitionDuration=".3s"
-      transitionTimingFunction="ease-in-out"
+      position="relative"
       backgroundColor="#18181b"
+      
     >
-      <Box color="white" maxWidth="1280px" margin="0 auto">
+      <Box color="white" maxWidth="1280px" margin="0 auto" >
         <HStack
-          px={16}
+          px={18}
           py={4}
           justifyContent="space-between"
           alignItems="center"
         >
           <nav>
-            {/* Add social media links based on the `socials` data */}
             <HStack spacing={18}>
               <a href={socials[0].url}>
                 <FontAwesomeIcon icon={socials[0].icon} size="2x" />
