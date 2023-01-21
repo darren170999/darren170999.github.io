35e0e4b9aee3d312dc605ebef97f33c3473af05c
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2023-01-21 10:02:53 +0800

    [Header]
    Added a header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import {ChakraProvider} from "@chakra-ui/react";
 import './App.css';
+import Header from "./components/Header";
 import LandingSection from "./components/LandingSection";
 
 function App() {
   return (
     <ChakraProvider>
+      <Header />
       <LandingSection />
 
     </ChakraProvider>
