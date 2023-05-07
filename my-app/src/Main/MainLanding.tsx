3cf669bfcade412881458e55af0c4f89746293d6
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2023-05-08 00:18:14 +0800

    remove useless things

--- my-app/src/Main/MainLanding.tsx
+++ my-app/src/Main/MainLanding.tsx
@@ -4,30 +4,7 @@ import Header from "./Components/Header";
 import InternshipsSection from "./Components/InternshipSection";
 import LandingSection from "./Components/LandingSection";
 import StacksSection from "./Components/StacksSection";
-import {
-  collection,
-  getDocs
-} from "firebase/firestore";
-import { db } from "../firebase-config";
-import { useEffect, useState } from "react";
-
-interface DataItem {
-  id: string;
-  // Add other properties here if needed
-}
-
 function MainPage() {
-  const [t, setT] = useState<DataItem[]>([])
-  const collectionRef = collection(db, "testing");
-
-  useEffect(()=> {
-    const getTest = async () => {
-      const data = await getDocs(collectionRef);
-      setT(data.docs.map((doc) => ({ ...doc.data(), id: doc.id } as DataItem)));
-    };
-
-    getTest();
-  }, []);
   return (
     <>
     <Header />
