a2c10410a9a5a278a8ba82762a7d1ec2355776a5
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2024-07-04 14:38:22 +0800

    Update BlogDatabricks.tsx

--- my-app/src/Main/Components/Blogs/BlogDatabricks.tsx
+++ my-app/src/Main/Components/Blogs/BlogDatabricks.tsx
@@ -68,26 +68,44 @@ function BlogDatabricks() {
                 </Stack>
                 <Stack padding="5" as="i">
                     <Text noOfLines={[20]} align="justify">
-                        During a recent event, I learned that Google is partnering with Databricks to create a new Data Intelligence Platform, which aims to simplify data 
-                        engineering processes by consolidating analytics applications onto one open cloud platform, enhancing collaboration, scalability, cost efficiency, 
+                        During a recent event (2024), I learned that Google is partnering with Databricks to create a new Data Intelligence Platform, which aims to simplify  
+                        data engineering processes by consolidating analytics applications onto one open cloud platform, enhancing collaboration, scalability, cost efficiency, 
                         and advanced analytics.
                     </Text>
                 </Stack>
                 <Stack padding="5">
                     <Text noOfLines={[20]} align="justify">
-                        {<Text as="b">TLDR:</Text>} Data Lakehouse Architecture is the way of the future. Delta Live Tables conveniently compresses early stages in data 
+                        {<Text as="b">TLDR:</Text>}  Data Lakehouse Architecture is the way of the future. Delta Live Tables conveniently compresses early stages in data 
                         pipelines into a single stage. Databricks makes Machine Learning and Data engineers' jobs alot easier as they allow versionings of datas, thread 
-                        management also seems to be integrated into the platform as well. So Data Scientists and Engineers can focus on the data and not worry about these
-                        issues.
-                        However, a personal note would be to understand how workflows are being orchestrated in the last stage of data pipelines. 
+                        management also seems to be integrated into the platform as well. So Data Scientists and Engineers can focus on the data analysis portions, greatly 
+                        reducing the need to worry about these issues.
+                        However, on a personal note, I should learn how workflows are being orchestrated in the last stage of data pipelines to better provide this summary. 
                     </Text>
+                    <Text noOfLines={[20]} align="justify">
+                        {<Text as="b"> Lakehouse's key features include: </Text>}
+                    </Text>
+                    <Text noOfLines={[20]} align="justify">
+                        1. Full ACID compliance with scalable data and metadata handling - a previously tedious process for data engineers.
+                    </Text>
+                    <Text noOfLines={[20]} align="justify">
+                        2. Audit History and Time Travelling - allowing for reproducible experiments and easy roll backs.
+                    </Text>
+                    <Text noOfLines={[20]} align="justify">
+                        3. Say no to data swamps - happens when too much bad data is ingested. (Bad data in = Bad data out)
+                    </Text>
+                    <Text noOfLines={[20]} align="justify">
+                        4. Efficient Data capture with unified streaming and batch processing.
+                    </Text>
+                </Stack>
+                <Stack padding="5">
                     <Text noOfLines={[20]} align="justify">
                         {<Text as="b">Overview: </Text>}
                         Overview: Databrick's main competive advantage lies in its ability to provide a new architecture known as the Data lakehouse, which is essentially a 
                         Data lake built on top of a Data warehouse. From the developers of Delta Lake, Apache Spark and MlFlow, Lakehouse was made to overcome a few engineering
-                        gaps, like giving structure to Data lake and having better runtime.
-                        In addition, Delta Live Tables will also value add to data engineering teams as they managed to streamline traditional data pipeline processes.
-
+                        gaps, like giving structure to Data lake and having better runtime. Lakehouse also uses delta lake and photon giving ACID support to database operations.
+                        In addition, Delta Live Tables will also value add to data engineering teams as they managed to streamline traditional data pipeline processes, making it 
+                        scaling of data and handling of metadata an easy feat. The ability to audit history and "time travel" back to previous versions also exists. This is done 
+                        through deleting updates and reversed merging.
                     </Text>
                     <Text noOfLines={[20]} align="justify">
                         {<Text as="b">Sample uses: </Text>}
@@ -101,7 +119,7 @@ function BlogDatabricks() {
                         be spun up to know the state of the workflow. Jobs can also run concurrently across a single workspace. Cells in Databricks notebooks also have thread managements.
                     </Text>
                     <Text noOfLines={[20]} align="justify">
-                        Lastly, (I have nothing to write for now.)
+                        Lastly, (I have nothing else to write for now.)
                     </Text>
                 </Stack>
                 <Stack padding="5">
