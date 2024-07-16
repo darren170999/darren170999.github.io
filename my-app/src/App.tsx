5f878f98ec2ca5aac63c3de393ca451ab9424ecf
DARREN SOH <61172383+darren170999@users.noreply.github.com>
2024-07-16 15:25:04 +0800

    Update App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,8 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {
-  ChakraProvider,
-  theme,
-} from "@chakra-ui/react";
+import { ChakraProvider, theme } from "@chakra-ui/react";
 import MainPage from './Main/MainPage';
 import BlogPage from "./Main/BlogPage";
 import BlogInternshipExperience from "./Main/Components/Blogs/BlogInternshipExperience";
@@ -11,30 +8,63 @@ import BlogDesignPatternsCSharp from "./Main/Components/Blogs/BlogDesignPatterns
 import BlogDatabricks from "./Main/Components/Blogs/BlogDatabricks";
 import BlogTemporal from "./Main/Components/Blogs/BlogTemporal";
 import BlogGarbageCollection from "./Main/Components/Blogs/BlogGarbageCollection";
+import { Profiler } from "react";
+
+// Define the blog routes
 const EngineeringBlogRoutes = () => (
   <Routes>
     <Route path="/" element={<BlogPage />} />
-    <Route path="Internships" element={<BlogInternshipExperience/>} />
-    <Route path="Sourcegraph" element={<BlogSourcegraphCody/>} />
-    <Route path="DesignPatterns" element={<BlogDesignPatternsCSharp/>}/>
-    <Route path="Databricks" element={<BlogDatabricks/>}/>
-    <Route path="Temporal" element={<BlogTemporal/>}/>
-    <Route path="GarbageCollection" element={<BlogGarbageCollection/>}/>
+    <Route path="Internships" element={<BlogInternshipExperience />} />
+    <Route path="Sourcegraph" element={<BlogSourcegraphCody />} />
+    <Route path="DesignPatterns" element={<BlogDesignPatternsCSharp />} />
+    <Route path="Databricks" element={<BlogDatabricks />} />
+    <Route path="Temporal" element={<BlogTemporal />} />
+    <Route path="GarbageCollection" element={<BlogGarbageCollection />} />
     {/* Add more sub-routes here as needed */}
   </Routes>
 );
+
+// Type definition for the onRender callback
+type ProfilerOnRenderCallback = (
+  id: string,
+  phase: 'mount' | 'update' | "nested-update",
+  actualDuration: number,
+  baseDuration: number,
+  startTime: number,
+  commitTime: number
+) => void;
+
+// Define the onRender callback function
+const onRenderCallback: ProfilerOnRenderCallback = (
+  id,
+  phase,
+  actualDuration,
+  baseDuration,
+  startTime,
+  commitTime
+) => {
+  console.log({
+    id,
+    phase,
+    actualDuration,
+    baseDuration,
+    startTime,
+    commitTime
+  });
+};
+
+// Define the main App component
 export function App() {
-
-  return(
-    <ChakraProvider theme={theme}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<MainPage/>}/>
-          <Route path="/EngineeringBlog/*" element={<EngineeringBlogRoutes/>}/>
-        </Routes>
-      </Router>
-
-    </ChakraProvider>
-  )
-
+  return (
+    <Profiler id="App" onRender={onRenderCallback}>
+      <ChakraProvider theme={theme}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/EngineeringBlog/*" element={<EngineeringBlogRoutes />} />
+          </Routes>
+        </Router>
+      </ChakraProvider>
+    </Profiler>
+  );
 }
